dadfc8457902df95a902b69e589574e5d688e3ec
unknown <sourabahmed546@gmail.com>
2022-01-09 00:19:15 +0600

    router-added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import NotFoundPage from './components/NotFoundPage/NotFoundPage';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Sourab Ahmed</h1>
+     <BrowserRouter>
+          <Routes>
+
+            <Route path="/" element={<Header />} />
+            <Route path="*" element={<NotFoundPage />} />
+
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Products/Products.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useCallback, useMemo } from "react";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { setProducts } from "../../../redux/actions/ProductActions";
+
+const Products = () => {
+  const products = useSelector((state) => state.allProducts.products);
+  const dispatch = useDispatch();
+  const fetchProducts = async () => {
+    const response = await axios
+      .get("https://fakestoreapi.com/products")
+      .catch((err) => {
+        console.log("Err: ", err);
+      });
+    dispatch(setProducts(response.data));
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log("Products :", products);
+  return (
+    <div className="ui grid container">
+        <h1>This is products</h1>
+    </div>
+  );
+};
+
+export default Products;
